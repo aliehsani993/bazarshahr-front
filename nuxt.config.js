11306de5b7d59ad7151366b52ac46ed9c4e21d53
sajjad rahimi <sajjadrahimi4@gmail.com>
2022-05-18 14:23:03 +0430

    fix nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
-
+  target:'static',
   srcDir: __dirname,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
